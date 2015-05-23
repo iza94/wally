636bd25312f7f1a04a40bf1455e98e0e778d07c7
Sileo1024 <natalia.niziolek@gmail.com>
2015-05-23 19:58:39 +0200

    blog settings added

--- app/views/blogs/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @blog, :id, :title, :user_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :blogs
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -7,6 +6,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'dashboard#index'
 
+  get 'blog/settings' => 'blogs#edit'
+  resources :blogs, only: [:index, :show, :edit, :update]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
