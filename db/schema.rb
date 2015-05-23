b9c7ced071e9d2da47fb6e5bc416102653892b59
Sileo1024 <natalia.niziolek@gmail.com>
2015-05-23 20:35:42 +0200

    generated a scaffold for a post, and making  navigation prettier

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150520194457) do
+ActiveRecord::Schema.define(version: 20150523180320) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
     t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "blog_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
